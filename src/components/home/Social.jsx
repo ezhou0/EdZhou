fb0bc2e1e65bc902b4d52f2908ae06debff8a581
Edwin <edwinzhou99@gmail.com>
2022-12-30 23:37:03 -0800

    add links to home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Social = () => {
+    return(
+        <div className="home__social">
+            <a href="https://www.linkedin.com/in/edwin-zhou-a231b31b6/" className="home__social-icon" target="_blank">
+                <i class="uil uil-linkedin"></i>
+            </a>
+
+            <a href="https://www.github.com/ezhou0" className="home__social-icon" target="_blank">
+                <i class="uil uil-github"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social;
