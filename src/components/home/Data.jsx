0fea3266f00c2d611e6e5d0a3d3e24ffd3b93049
Edwin <edwinzhou99@gmail.com>
2022-12-31 13:51:50 -0800

    add landing about and animations

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Data = () => {
+    return(
+        <div className="home__data">
+            <h1 className="home__title">Edwin Zhou
+
+            </h1>
+            <h3 className="home__subtitle"> Software Engineer </h3>
+            <p className="home__description">Recent UCLA Grad based in Seattle</p>
+
+           <a href="#contact" className="button button--flex">Contact me
+            </a>
+
+        </div>
+    )
+}
+
+export default Data;
