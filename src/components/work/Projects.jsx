8247260db43609ccd0bed07a7bdbe2b7276caa10
Edwin <edwinzhou99@gmail.com>
2023-01-01 00:40:26 -0800

    add tab functionality to projects

--- src/components/work/Projects.jsx
+++ src/components/work/Projects.jsx
@@ -13,20 +13,33 @@ const Projects = () => {
         if(item.name === "all"){
             setProjects(projectsData)
         }
-    })
+        else{
+            const newProjects = projectsData.filter((project) =>{
+                return project.category === item.name;
+            });
+            setProjects(newProjects);
+        }
+    }, [item]);
+
+    const handleClick = (e, index) => {
+        setItem({name: e.target.textContent});
+        setActive(index);
+    }
 
     return (
         <div>
             <div className="work__filters">
                 {projectsNav.map((item, index) => {
                     return(
-                        <span className="work__item" key={index}>{item.name}</span>
+                        <span className={`${active === index ? 'active-work' : ""} work__item`}  key={index} onClick={(e)=>{
+                            handleClick(e, index);
+                        }}>{item.name}</span>
                     )
                 })};
             </div>
 
             <div className="work__container container grid">
-                {projectsData.map((item) => {
+                {projects.map((item) => {
                     return <WorkItems item={item} key={item.id} />
                 })}
             </div>
