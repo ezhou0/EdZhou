90dc6ab065f19dab4cf49496c3862984d087d0eb
Edwin <edwinzhou99@gmail.com>
2023-07-06 16:49:42 -0700

    add modal component

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './modal.css';
+
+const Modal = () => {
+    return(
+        <div>
+            Under Maintanence due to Heroku changing its hosting conditions. Working on updating transferring to a new hosting service!
+        </div>
+    )
+}
+export default Modal;
